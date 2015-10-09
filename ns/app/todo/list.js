7165d404a45ba347fc328b0c75e2ff1692365ed0
Nedyalko Nikolov <nedyalko.nikolov@telerik.com>
2015-10-09 10:31:19 +0300

    Added todoViewModel module.

--- ns/app/todo/list.js
+++ ns/app/todo/list.js
@@ -4,10 +4,10 @@ var frameModule = require("ui/frame");
 var viewModule = require("ui/core/view");
 var observableModule = require("data/observable");
 var observableArray = require("data/observable-array");
+var todoViewModel = require("./todoViewModel");
+var platform = require("platform");
 
-var _ = require('underscore');
 var swipeDelete = require("../utils/ios-swipe-delete");
-var config = require('./../config');
 
 var topmost;
 
@@ -17,134 +17,95 @@ var onPressCheckbox;
 var markAllAsDoneOnTap;
 
 exports.navigatedTo = function(args) {
-  var page = args.object;
-  var todoItems;
-
-  var pageData = new observableModule.Observable();
-  page.bindingContext = pageData;
-
-  var listView = page.getViewById("listView");
-
-  if (!topmost) {
-    topmost = frameModule.topmost();
-  }
-
-  pageData.set("showBack", topmost.canGoBack());
-
-  if (args.object.todoItems) {
-    todoItems = args.object.todoItems;
-  } else {
-    if (page.navigationContext && page.navigationContext.todoItems) {
-      pageData.set("showBack", true);
-      todoItems = page.navigationContext.todoItems;
-    } else {
-      todoItems = new observableArray.ObservableArray([]);
+	page = args.object;
+    var viewModel;
+    if (page.navigationContext) {
+        viewModel = page.navigationContext;
     }
-    args.object.todoItems = todoItems;
-  }
+    else {
+        viewModel = new todoViewModel.ToDoViewModel(null);
+    }
+    page.bindingContext = page.bindingContext || viewModel;
 
-  pageData.set("todoItems", todoItems);
+    var listView = page.getViewById("listView");
 
-  if (page.ios) {
-    var iosFrame = frameModule.topmost().ios;
-    iosFrame.navBarVisibility = "never";
-    iosFrame.controller.view.window.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(0.96, 0.96, 0.96, 1);
+	if (!topmost) {
+		topmost = frameModule.topmost();
+	}
+	
+	page.bindingContext.set("showBack", topmost.canGoBack());
 
-    // TODO: Replace with a "returnKey" listener in 1.4.
-    // See https://github.com/NativeScript/sample-Groceries/issues/17
-    application.ios.addNotificationObserver("UITextFieldTextDidEndEditingNotification", createRow);
+    if (platform.device.os === platform.platformNames.ios) {
+		var iosFrame = topmost.ios;
+		iosFrame.navBarVisibility = "never";
+		iosFrame.controller.view.window.backgroundColor = UIColor.colorWithRedGreenBlueAlpha(0.96, 0.96, 0.96, 1);
 
-    // TODO: This solution is very specific to deleting and doesn't handle other swipe actions.
-    // UI for NativeScript has a built-in solution for this coming in the next week or so.
-    // If I get my hands on it in time I can throw that in here.
+		// TODO: Replace with a "returnKey" listener in 1.4.
+		// See https://github.com/NativeScript/sample-Groceries/issues/17
+		application.ios.addNotificationObserver("UITextFieldTextDidEndEditingNotification", createRow);
+
+		// TODO: This solution is very specific to deleting and doesn't handle other swipe actions.
+		// UI for NativeScript has a built-in solution for this coming in the next week or so.
+		// If I get my hands on it in time I can throw that in here.
     
-    swipeDelete.enable(listView, function(index) {
-        todoItems.splice(index, 1);
-    });
-  }
+		swipeDelete.enable(listView, function(index) {
+			page.bindingContext.get("todoItems").splice(index, 1);
+    	});
+	}
+}
     
-  function updateRowChildren(rowID, children) {
-    if (todoItems.getItem(rowID)) {
-      var item = todoItems.getItem(rowID);
-      item.children = children;
-      todoItems.setItem(rowID, item);
-    }
-  }
+function createRow(args) {
+	var bindContext = page.bindingContext;
+	var itemText = bindContext.textInput;
+	if (itemText) {
+		bindContext.todoItems.unshift(new todoViewModel.TodoItem(itemText, false));
+		bindContext.textInput = "";
+	}
+}
 
-  function createRow(args) {
-    var text = pageData.get('textInput');
-    if (text) {
-      page.getViewById("textInput").focus();
-
-      todoItems.unshift(_.extend({
-        text: text,
-        children: new observableArray.ObservableArray([])
-      }, config.rowTypes.notDone));
-
-      if (page.navigationContext && page.navigationContext.updateRowChildren) {
-        page.navigationContext.updateRowChildren(page.navigationContext.rowID, todoItems);
-      }        
-      
-      pageData.set("textInput", "");
-    }
-  }
-
-  function markAllAsDone(_todoItems, newParams) {
-    newParams = newParams || config.rowTypes.done;
+function markAllAsDone(_todoItems, newParams) {
+    newParams = newParams === undefined ? true : newParams;
     _todoItems.forEach(function(item, index) {
 
-      if (item.children && item.children.length > 0) {
-        item.children = markAllAsDone(item.children, newParams);
-      }
+	if (item.children && item.children.length > 0) {
+		markAllAsDone(item.children, newParams);
+    }
+	  
+	item.isChecked = newParams;
 
-      console.log(JSON.stringify(newParams));
+	});
+}
 
-      // ObservableArray's support JS Array.prototype methods
-      _todoItems.setItem(index, _.extend({
-        text: item.text,
-        children: item.children
-      }, newParams));         
-    });
+markAllAsDoneOnTap = function () {
+	markAllAsDone(page.bindingContext.get("todoItems"));
+}; 
 
-    return _todoItems;
-  }
-
-  markAllAsDoneOnTap = function () {
-    todoItems = markAllAsDone(todoItems);
-    args.object.todoItems = todoItems;
-  };   
-
-  rowOnPress = function(args) {
+rowOnPress = function(args) {
+	var bindContext = page.bindingContext;
     topmost.navigate({
-      moduleName: "todo/list",
-      context: {
-        rowID: args.index,
-        updateRowChildren: updateRowChildren,
-        todoItems: todoItems.getItem(args.index).children
-      }
-    });        
-  }
+        moduleName: "todo/list",
+        context: new todoViewModel.ToDoViewModel(bindContext.todoItems.getItem(args.index).children)
+    });
+};
 
-  onPressCheckbox = function(args) {
+onPressCheckbox = function(args) {
     var currentItem = args.view.bindingContext;
-
-    var index = todoItems.indexOf(currentItem);
+	
+	var _todoItems = page.bindingContext.get("todoItems");
+    var index = _todoItems.indexOf(currentItem);
     var toUpdate = {};
     
+	console.log("currentItem: " + currentItem);
+	
     if (currentItem.isChecked) {
-        toUpdate = config.rowTypes.notDone;
+		toUpdate = false;
     } else {
-        toUpdate = config.rowTypes.done; 
+		toUpdate = true;
     }     
 
-    todoItems.setItem(index, _.extend({
-        text: currentItem.text,
-        children: currentItem.children
-    }, toUpdate));
+	_todoItems.getItem(index).isChecked = toUpdate;
 
-    markAllAsDone(currentItem.children, toUpdate);
-  };
-
+    markAllAsDone(_todoItems.getItem(index).children, toUpdate);
 };
 
 exports.onBackTap = function() {
